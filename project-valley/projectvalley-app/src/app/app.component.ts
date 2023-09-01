19c97bdb5d46d1255e398b62ef08daabea44355c
Nomfundo Florence <nomfundomlangeni0@gmail.com>
2023-09-01 18:07:25 +0200

    Functionality

--- project-valley/projectvalley-app/src/app/app.component.ts
+++ project-valley/projectvalley-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+//import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projectvalley-app';
+
+  onSubmit() {
+    // This method will be called when the form is submitted
+    // You can handle form submission logic here
+    console.log('Form submitted!');
+  }
+
+  resetForm(form: NgForm) {
+    form.resetForm(); // Reset the form to its initial state
+  }
 }
